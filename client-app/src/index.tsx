8ec933de4b2a39c8014656451f8010f01df5bc52
Mike Armitage <mike.armitage@barsquared.com>
2020-05-20 12:28:58 +0100

    End of section 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './layout/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./layout/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./app/layout/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- client-app/src/layout/App.tsx
+++ client-app/src/layout/App.tsx
@@ -1,31 +1,38 @@
-import React, { useEffect, Fragment, useContext } from "react";
+import React, { Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import "./styles.css";
 import NavBar from "../app/features/nav/NavBar";
 import ActivityDashboard from "../app/features/nav/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "../app/layout/LoadingComponent";
-import ActivityStore from "../app/stores/activityStore";
 import { observer } from "mobx-react-lite";
+import { Route, withRouter, RouteComponentProps } from "react-router-dom";
+import HomePage from "../app/features/home/HomePage";
+import ActivityForm from "../app/features/nav/activities/form/ActivityForm";
+import ActivityDetails from "../app/features/nav/activities/details/ActivityDetails";
 
-const App = () => {
-  const activityStore = useContext(ActivityStore);
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading activities..." />;
-
+const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
     <Fragment>
-      <NavBar />
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <Fragment>
+            <NavBar />
 
-      <Container style={{ paddingTop: "7em" }}>
-        <ActivityDashboard />
-      </Container>
+            <Container style={{ paddingTop: "7em" }}>
+              <Route exact path="/activities" component={ActivityDashboard} />
+              <Route path="/activities/:id" component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={["/createActivity", "/manage/:id"]}
+                component={ActivityForm}
+              />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 };
 
-export default observer(App);
+export default withRouter(observer(App));
